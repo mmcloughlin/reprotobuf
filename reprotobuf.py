f52229f30394d96e818481f0ce829de76396ad44
Michael McLoughlin <mmcloughlin@gmail.com>
2015-06-06 21:47:34 -0700

    Some cleanup of main class

--- reprotobuf.py
+++ reprotobuf.py
@@ -35,6 +35,11 @@ class Reprotobuf(object):
                     'abstract' not in cls.get_access_flags_string())
         return filter(is_proto, self.dvm.get_classes())
 
+
+class MessageNanoAnalyzer(object):
+    def __init__(self, workspace):
+        self.workspace = workspace
+
     def get_fields_from_class(self, cls):
         """
         Deduce fields by inspecting the fields of the Java class.
@@ -66,7 +71,7 @@ class Reprotobuf(object):
         if len(methods) == 0:
             return {}
 
-        method = self.vma.get_method(methods[0])
+        method = self.workspace.vma.get_method(methods[0])
         basic_blocks = method.basic_blocks.gets()
 
         e = executor.WriteToExecutor()
@@ -77,44 +82,29 @@ class Reprotobuf(object):
 
         return e.get_tags()
 
+    def analyze(self, cls):
+        # deduce fields
+        fields = self.get_fields_from_class(cls)
+        # deduce tags
+        tag_map = self.get_tags_from_class(cls)
+        for name, tag in tag_map.items():
+            assert name in fields
+            fields[name]['tag'] = tag
+        # check we got a tag for everything
+        for properties in fields.values():
+            assert 'tag' in properties
+        return fields
+
+
 # main ---------------------------------------------------------
 
-rpb = Reprotobuf.from_classes_dex(sys.argv[1])
-proto_classes = rpb.get_proto_classes()
+workspace = Reprotobuf.from_classes_dex(sys.argv[1])
+analyzer = MessageNanoAnalyzer(workspace)
+proto_classes = workspace.get_proto_classes()
 
 for cls in proto_classes:
-    print '>>>>>>>>', cls.get_name()
-    # deduce fields
-    fields = rpb.get_fields_from_class(cls)
-    # deduce tags
-    tag_map = rpb.get_tags_from_class(cls)
-    for name, tag in tag_map.items():
-        assert name in fields
-        fields[name]['tag'] = tag
-    # report
-    tags = set()
+    print '>', cls.get_name()
+    fields = analyzer.analyze(cls)
     for name, properties in fields.items():
-        for k, v in properties.items():
-            print '%s=%s' % (k, v),
-        if 'tag' in properties:
-            tags.add(properties['tag'])
-        else:
-            print 'ERROR:TAG_MISSING',
-        print
-
-    if len(tags) < len(fields):
-        print 'ERROR:TAGS_MISSING'
-    if len(tags) == 0:
-        print 'ERROR:ZERO_TAGS'
-        continue
-    if min(tags) != 1 or max(tags) != len(tags):
-        print 'ERROR:TAGRANGE'
-        upper = max(max(tags), len(tags))
-        for t in range(1, upper+1):
-            print '%2d' % (t),
-        print
-        for t in range(1, upper+1):
-            print '..' if t in tags else 'xx',
-        print
+        print '  %(name)40s %(tag)5d %(required)6s %(descriptor)s' % properties
     print
-
